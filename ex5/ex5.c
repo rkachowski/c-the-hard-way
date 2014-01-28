17b4a10aab908e79218d52f5a93bc538a5b8d167
Donald Hutchison <git@toastymofo.net>
2014-01-28 22:11:08 +0100

    Exercise 5.

---
+++ ex5/ex5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+    int distance = 100;
+
+    //whoopty do a comment oh wow
+    printf("You are %d miles away.\n", distance);
+    return 0;
+}
