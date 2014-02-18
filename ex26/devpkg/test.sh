25a97945c3b80a4d60e28d7053f1bc2c2eee4324
Donald Hutchison <git@toastymofo.net>
2014-02-18 17:40:02 +0100

    Test script.

--- ex26/devpkg/test.sh
+++ ex26/devpkg/test.sh
@@ -7,3 +7,24 @@ fi
 
 #test init
 ./devpkg -S
+
+echo "-S (init) works!"
+
+./devpkg -I http://archive.apache.org/dist/apr/apr-1.4.6.tar.gz
+
+echo "-I (install) works!"
+
+./devpkg -L
+
+echo "-L (list) works!"
+
+./devpkg -F https://github.com/rkachowski/fag-blaster.git
+
+echo "-F (fetch) works!"
+
+./devpkg -I http://archive.apache.org/dist/apr/apr-1.4.6.tar.gz
+
+echo "-B (build) works!"
+
+
+echo "If you can see this, everything is peachy."
