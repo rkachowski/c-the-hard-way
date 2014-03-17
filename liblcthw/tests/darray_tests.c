ac40d04808a37782c47f74430cc49a28ada20886
Donald Hutchison <git@toastymofo.net>
2014-03-17 11:43:37 +0100

    Fix bug.

--- liblcthw/tests/darray_tests.c
+++ liblcthw/tests/darray_tests.c
@@ -98,7 +98,7 @@ char *test_push_pop()
         int *val = DArray_pop(array);
         mu_assert(val != NULL, "Shouldn't get a NULL");
         mu_assert(*val == i *333, "Wrong value");
-        DArray_free(val)
+        DArray_free(val);
     }
 
     return NULL;
